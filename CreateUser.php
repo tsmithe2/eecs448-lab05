a6055baa2dc6ad3a2b5b74e3bd63114edf7db9fb
Thomas <thomas_smithey@yahoo.com>
2019-03-30 13:43:14 -0500

    new code and testing

---
+++ CreateUser.php
@@ -0,0 +1,28 @@
+<?php
+	$user_name = $POST["username"];
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "t340s709", "ke3ve4Ni", "t340s709");
+	$null = "";
+	$erMessage = "An error occured: ";
+
+	if ($mysqli->connect_errno)
+	{
+		printf("connect failed");
+	}
+	if ($username == $null)
+	{
+		echo "<p>" . $erMessage . "You must enter a username.</p>";
+	}
+	else
+	{
+		$query = "INSERT INTO Users (user_id) VALUES ('" . $user_name . "')";
+		if ($mysqli->query($query))
+		{
+			echo "<p>" . $user_name . " was created and stored.</p>";
+		}
+		else
+		{
+			echo "<p>" . $erMessage . $user_name . " has already been created</p>";
+		}
+	}
+	$mysqli->close();
+?>
